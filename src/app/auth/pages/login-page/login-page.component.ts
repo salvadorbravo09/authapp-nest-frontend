84ea33ccfdf35b3a3a38b1ea771a371f7a0b0681
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-10-31 23:15:54 -0300

    add pages for auth

---
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -0,0 +1,20 @@
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrl: './login-page.component.css',
+})
+export class LoginPageComponent {
+  private fb = inject(FormBuilder);
+
+  public myForm: FormGroup = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  login() {
+    console.log(this.myForm.value)
+  }
+}
